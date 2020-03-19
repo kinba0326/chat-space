cf91bddef14b777db4bd9b4d306c41a48432d63d
kinba0326 <kinba.tsukasa@icloud.com>
2020-03-19 13:00:52 +0900

    setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,5 @@
 server '52.198.100.247', user: 'ec2-user', roles: %w{app db web}
 
-
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
